f7821f73767c1958d93e631c05386ae4dec80639
mila <milavoronina1997@mail.ru>
2024-02-22 21:32:04 +0500

    task class created

---
+++ src/main/java/ru/gb/voronina/SpringHW5/model/Task.java
@@ -0,0 +1,25 @@
+package ru.gb.voronina.SpringHW5.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    private LocalDateTime dateCreate;
+}
---
+++ src/main/java/ru/gb/voronina/SpringHW5/model/TaskStatus.java
@@ -0,0 +1,6 @@
+package ru.gb.voronina.SpringHW5.model;
+
+public enum TaskStatus {
+
+    NOT_STARTED, IN_PROGRESS, COMPLETED
+}
